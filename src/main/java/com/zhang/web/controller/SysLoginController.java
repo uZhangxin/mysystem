646ccb3c3184eadf70c64129957df086956e5173
newZhang <626587479@qq.com>
2022-08-10 17:09:51 +0800

    feat: 新增简单登录验证接口并以统一响应对象返回操作结果

---
+++ src/main/java/com/zhang/web/controller/SysLoginController.java
@@ -0,0 +1,43 @@
+package com.zhang.web.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.zhang.web.common.AjaxResult;
+import com.zhang.web.common.CommonInfo;
+import com.zhang.web.common.HttpStatus;
+import com.zhang.web.entity.SysUser;
+import com.zhang.web.model.LoginBody;
+
+import com.zhang.web.service.ISysUserService;
+import com.zhang.web.service.SysLoginService;
+import org.apache.ibatis.jdbc.Null;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: zhangxin
+ * @Date: 2022/8/10 9:58
+ * @Description:
+ */
+@RestController
+public class SysLoginController {
+
+    @Autowired
+    ISysUserService userService;
+
+    @PostMapping("/login")
+    public AjaxResult login(LoginBody loginBody) {
+        AjaxResult ajaxResult = AjaxResult.success();
+        QueryWrapper<SysUser> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("user_name", loginBody.getUserName())
+                .eq("password", loginBody.getPassword());
+        SysUser user = userService.getOne(queryWrapper);
+        if (user == null) {
+            ajaxResult.put("code", HttpStatus.ACCEPTED);
+            ajaxResult.put("msg","用户名密码不正确");
+            return ajaxResult;
+        }
+        return AjaxResult.success();
+    }
+}
