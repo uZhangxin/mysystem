19a490a31433cb5e6299d51d915932842eef4896
newZhang <626587479@qq.com>
2022-08-22 16:00:12 +0800

    refactor: 修改项目结构

---
+++ admin/src/main/java/com/zhang/web/service/SysLoginService.java
@@ -0,0 +1,16 @@
+package com.zhang.web.service;
+
+import com.zhang.web.common.AjaxResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author: zhangxin
+ * @Date: 2022/8/10 10:18
+ * @Description:
+ */
+@Component
+public class SysLoginService {
+
+}
